cdd21f2c8ad7b473251d9794b3ae3c5581e96034
Liam Collins <jeliroco333@gmail.com>
2023-04-09 23:42:40 -0400

    manually set style to avoid {...rest} error on CustomGrid

--- components/CustomGrid.js
+++ components/CustomGrid.js
@@ -11,12 +11,22 @@ import { TinaTemplate } from "./TinaTemplate";
 export const CustomGrid = (props) => {
   const { gridItems, children, ...rest } = props;
   const style = cleanupStyle(props.style);
-  const numItems = useBreakpointValue(style?.numItems ?? {base: Number.MAX_SAFE_INTEGER});
+  const numItems = useBreakpointValue(
+    style?.numItems ?? { base: Number.MAX_SAFE_INTEGER }
+  );
 
   return (
-    <Grid w="100%" gap={4} {...style}>
+    <Grid
+      w="100%"
+      gap={4}
+      gridTemplateRows={style?.gridTemplateRows}
+      gridTemplateColumns={style?.gridTemplateColumns}
+    >
       {gridItems?.map((gridItem, i) => {
-        if (i < (numItems != null ? parseFloat(numItems) : Number.MAX_SAFE_INTEGER)) {
+        if (
+          i <
+          (numItems != null ? parseFloat(numItems) : Number.MAX_SAFE_INTEGER)
+        ) {
           return <TinaTemplate key={i} template={gridItem} />;
         }
       })}
